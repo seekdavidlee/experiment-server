695e94776840090d0145c20f8419f5089204f697
David Lee <seekdavidlee@gmail.com>
2025-03-15 10:10:36 -0500

    add initial comparisons

--- src/Pages/Experiments/ExperimentsComparison.razor.cs
+++ src/Pages/Experiments/ExperimentsComparison.razor.cs
@@ -20,46 +20,143 @@ public partial class ExperimentsComparison
     [Inject]
     private NavigationManager? NavigationManager { get; set; }
 
-    private CompareTableModel? Inputs;
+    [Inject]
+    private FileSystemApi? Client { get; set; }
 
-    protected override Task OnInitializedAsync()
+    [Inject]
+    private ILogger<ExperimentsComparison>? Logger { get; set; }
+
+    private CompareTableModel? Inputs;
+    private CompareTableModel? FieldsAccuracy;
+
+    protected override async Task OnInitializedAsync()
     {
         if (UserSession!.Items.TryGetValue(nameof(ExperimentsComparison), out var experimentRunsObj))
         {
-            var accessor = TypeAccessor.Create(typeof(ExperimentRun));
-            var members = accessor.GetMembers();
-            List<string> props = [
-                nameof(ExperimentRun.ModelId),
+            var runs = (List<ExperimentRun>)experimentRunsObj;
+
+            InitInputs(runs);
+            await InitFieldsAccuracyAsync(runs);
+        }
+    }
+
+    private void InitInputs(List<ExperimentRun> runs)
+    {
+        var accessor = TypeAccessor.Create(typeof(ExperimentRun));
+        List<string> props = [
+            nameof(ExperimentRun.ModelId),
                 nameof(ExperimentRun.SystemPrompt),
                 nameof(ExperimentRun.UserPrompt),
                 nameof(ExperimentRun.Temperature),
                 nameof(ExperimentRun.TopP),
                 ];
 
-            List<string> colNames = [];
-            Dictionary<string, CompareTableRow> rows = [];
-            foreach (var experimentRun in (List<ExperimentRun>)experimentRunsObj)
+        List<string> colNames = [];
+        Dictionary<string, CompareTableRow> rows = [];
+        foreach (var experimentRun in runs)
+        {
+            colNames.Add(experimentRun.Id.ToString());
+            foreach (var p in props)
             {
-                colNames.Add(experimentRun.Id.ToString());
-                foreach (var p in props)
+                if (!rows.TryGetValue(p, out var row))
                 {
-                    if (!rows.TryGetValue(p, out var row))
+                    row = new CompareTableRow { Name = p, Cells = [] };
+                    rows.Add(p, row);
+                }
+
+                row.Cells!.Add(new CompareTableCell { Value = accessor[experimentRun, p].ToString() });
+            }
+        }
+
+        Inputs = new CompareTableModel { ColumnNames = [.. colNames], Rows = [.. rows.Values] };
+    }
+
+    private async Task InitFieldsAccuracyAsync(List<ExperimentRun> runs)
+    {
+        List<string> colNames = [];
+        Evaluation eval = new();
+        Dictionary<string, CompareTableRow> rows = [];
+        foreach (var experimentRun in runs)
+        {
+            colNames.Add(experimentRun.Id.ToString());
+            Dictionary<string, FieldAccuracy> fields = [];
+
+            var results = await Client!.GetExperimentRunResultsAsync(ProjectId, ExperimentId, experimentRun.Id);
+            if (results is null)
+            {
+                Logger!.LogError("unable to get experiment run");
+                return;
+            }
+
+            var metrics = await Client!.GetExperimentRunMetricsAsync(ProjectId, ExperimentId, experimentRun.Id);
+            if (metrics is null)
+            {
+                Logger!.LogError("unable to get experiment run metrics");
+                return;
+            }
+
+            foreach (var res in results)
+            {
+                var metric = metrics.SingleOrDefault(x => x.ResultId == res.Id);
+                if (metric is null)
+                {
+                    Logger!.LogError("unable to get metric for result");
+                    return;
+                }
+
+                if (!metric.Meta!.TryGetValue("image_file_path", out var imagePathObj))
+                {
+                    Logger!.LogError("unable to get image file path");
+                    return;
+                }
+
+                var groundTruthResponse = await Client.GetJsonAsync<GroundTruthImage>(imagePathObj.Replace(".jpg", ".json"));
+                if (!groundTruthResponse.Success)
+                {
+                    Logger!.LogError("unable to get ground truth json");
+                    return;
+                }
+
+                foreach (var assertion in eval.GetAssertions(groundTruthResponse.Result!, res.Text!))
+                {
+                    if (!fields.TryGetValue(assertion.Field, out var fieldAcc))
                     {
-                        row = new CompareTableRow { Name = p, Cells = [] };
-                        rows.Add(p, row);
+                        fieldAcc = new FieldAccuracy { Name = assertion.Field };
+                        fields.Add(assertion.Field, fieldAcc);
                     }
 
-                    row.Cells!.Add(new CompareTableCell { Value = accessor[experimentRun, p].ToString() });
+                    fieldAcc.Total++;
+                    if (assertion.Success)
+                    {
+                        fieldAcc.Correct++;
+                    }
                 }
             }
 
-            Inputs = new CompareTableModel { ColumnNames = [.. colNames], Rows = [.. rows.Values] };
+            foreach (var field in fields.Values)
+            {
+                if (!rows.TryGetValue(field.Name!, out var row))
+                {
+                    row = new CompareTableRow { Name = field.Name, Cells = [] };
+                    rows.Add(field.Name!, row);
+                }
+                decimal accuracy = field.Total == 0 ? 0 : (decimal)field.Correct / field.Total;
+                row.Cells!.Add(new CompareTableCell { Value = String.Format("{0:P3}", accuracy) });
+            }
         }
-        return base.OnInitializedAsync();
+
+        FieldsAccuracy = new CompareTableModel { ColumnNames = [.. colNames], Rows = [.. rows.Values] };
     }
 
     private void Back()
     {
         NavigationManager!.NavigateTo($"/projects/{ProjectId}/experiments/{ExperimentId}/runs");
     }
+}
+
+public class FieldAccuracy
+{
+    public string? Name { get; set; }
+    public int Correct { get; set; }
+    public int Total { get; set; }
 }
---
+++ src/Shared/TableView.razor.cs
@@ -0,0 +1,12 @@
+﻿using ExperimentServer.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace ExperimentServer.Shared;
+
+public partial class TableView
+{
+    [Parameter]
+    public string? Title { get; set; }
+    [Parameter]
+    public CompareTableModel? TableModel { get; set; }
+}
